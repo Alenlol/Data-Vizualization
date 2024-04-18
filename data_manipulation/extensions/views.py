291d943a1ba046144a302c680095640723d8335e
Alysh <alen.light@bk.ru>
2024-04-18 09:56:04 +0500

    Creating upload file form and saving it into documents sorting by date

--- data_manipulation/extensions/views.py
+++ data_manipulation/extensions/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
+from .models import Document
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'extensions/home.html')
+    form = UploadFileForm()
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['file'])
+            newdoc.save()
+
+            messages.success(request, 'Successfully uploaded')
+
+            print(1)
+        else:
+            print(0)
+        return redirect('home')
+
+    context = {'form': form}
+
+    return render(request, 'extensions/home.html', context)
 
